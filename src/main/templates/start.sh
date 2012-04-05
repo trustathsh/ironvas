872793d62c13d76e412a9036170a3821d717c9ad
Ralf Steuerwald <ralf.steuerwald@stud.fh-hannover.de>
2012-04-05 16:20:44 +0000

    ironvas: move config templates to src/main/templates
    git-svn-id: https://proanvil.inform.fh-hannover.de/ifmap/svn/trunk/sourcecode/ironvas@1822 ad3e5548-70bb-4284-8a10-7354a74352f5

---
+++ src/main/templates/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+CLASSPATH=".:"\
+"original-ironvas-0.0.4.jar:"\
+"lib/httpcore-4.1.1.jar:"\
+"lib/ifmapj-0.1.4.jar:"\
+"lib/scala-library-2.9.0.jar"
+
+/usr/bin/java -classpath $CLASSPATH  de.fhhannover.inform.trust.ironvas.Ironvas
