338239c2e4fcf612c0c73b446ce80e582603d365
Ralf Steuerwald <ralf.steuerwald@hs-hannover.de>
2013-11-11 12:11:47 +0100

    licenses: update licenses header in .java files

--- src/main/java/de/fhhannover/inform/trust/ironvas/Ironvas.java
+++ src/main/java/de/fhhannover/inform/trust/ironvas/Ironvas.java
@@ -1,25 +1,48 @@
+package de.fhhannover.inform.trust.ironvas;
+
 /*
- * Project: ironvas
- * Package: de.fhhannover.inform.trust.ironvas
- * File:    Ironvas.java
- *
- * Copyright (C) 2011-2012 Hochschule Hannover
+ * #%L
+ * ====================================================
+ *   _____                _     ____  _____ _   _ _   _
+ *  |_   _|_ __ _   _ ___| |_  / __ \|  ___| | | | | | |
+ *    | | | '__| | | / __| __|/ / _` | |_  | |_| | |_| |
+ *    | | | |  | |_| \__ \ |_| | (_| |  _| |  _  |  _  |
+ *    |_| |_|   \__,_|___/\__|\ \__,_|_|   |_| |_|_| |_|
+ *                             \____/
+ * 
+ * =====================================================
+ * 
+ * Fachhochschule Hannover 
+ * (University of Applied Sciences and Arts, Hannover)
+ * Faculty IV, Dept. of Computer Science
  * Ricklinger Stadtweg 118, 30459 Hannover, Germany
- *
- * Licensed under the Apache License, Version 2.0 (the License);
+ * 
+ * Email: trust@f4-i.fh-hannover.de
+ * Website: http://trust.inform.fh-hannover.de/
+ * 
+ * This file is part of ironvas, version 0.1.1, implemented by the Trust@FHH 
+ * research group at the Fachhochschule Hannover.
+ * 
+ * ironvas is a *highly experimental* integration of Open Vulnerability Assessment 
+ * System (OpenVAS) into a MAP-Infrastructure. The integration aims to share security 
+ * related informations (vulnerabilities detected by OpenVAS) with other network 
+ * components in the TNC architecture via IF-MAP.
+ * %%
+ * Copyright (C) 2011 - 2013 Trust@FHH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an AS IS BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * #L%
  */
-
-package de.fhhannover.inform.trust.ironvas;
 
 import java.io.IOException;
 import java.io.InputStream;
