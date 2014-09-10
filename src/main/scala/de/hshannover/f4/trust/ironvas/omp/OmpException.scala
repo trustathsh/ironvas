a198d6b740558e5eb18f56e733cbe2b65f8ac012
Bastian Hellmann <bahellma@googlemail.com>
2014-09-10 11:37:35 +0200

    [release] version 0.1.3

--- src/main/scala/de/hshannover/f4/trust/ironvas/omp/OmpException.scala
+++ src/main/scala/de/hshannover/f4/trust/ironvas/omp/OmpException.scala
@@ -18,7 +18,7 @@
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de
  * 
- * This file is part of ironvas, version 0.1.2, implemented by the Trust@HsH
+ * This file is part of ironvas, version 0.1.3, implemented by the Trust@HsH
  * research group at the Hochschule Hannover.
  * %%
  * Copyright (C) 2011 - 2014 Trust@HsH
